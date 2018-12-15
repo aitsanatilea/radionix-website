d4e089596533eefb273cc41f39d0476f2844db70
Anna <a.aleksieienko@gmail.com>
2018-12-16 00:17:40 +0100

    Fix for RU menu

--- source/_partials/menu_ru.blade.php
+++ source/_partials/menu_ru.blade.php
@@ -3,11 +3,11 @@
   <div class="inner">
     <h2>Меню</h2>
     <ul class="links">
-      <li><a href="/en/index.html">Главная</a></li>
-      <li><a href="/en/about.html">О компании</a></li>
-      <li><a href="/en/products.html">Продукция</a></li>
-      <li><a href="/en/index.html#projects">Проекты</a></li>
-      <li><a href="/en/index.html#contact">Контакты</a></li>
+      <li><a href="/index.html">Главная</a></li>
+      <li><a href="/about.html">О компании</a></li>
+      <li><a href="/products.html">Продукция</a></li>
+      <li><a href="/index.html#projects">Проекты</a></li>
+      <li><a href="/index.html#contact">Контакты</a></li>
     </ul>
     <a href="#" class="close">Закрыть</a>
   </div>
